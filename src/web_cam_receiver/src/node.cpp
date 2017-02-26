ff7d6b75c607d5dbaa782064197a418d30692821
Michael Kopp <kopp.michael@yahoo.de>
2017-02-26 17:14:15 +0100

    web_cam: configure using ros params

--- src/web_cam_receiver/src/node.cpp
+++ src/web_cam_receiver/src/node.cpp
@@ -223,6 +223,20 @@ public:
 };
 
 
+/// \brief helper to load parameters
+#define getParamFatal(param_name, description) \
+    std::string param_name; \
+    if (! nh.getParam(#param_name, param_name)) \
+    { \
+      ROS_ERROR("please specify " description " as parameter " #param_name); \
+      all_params_available = false; \
+    } \
+    else \
+    { \
+      ROS_INFO_STREAM("Using " #param_name " = " << param_name); \
+    }
+
+
 int main(int argc, char** argv)
 {
   try
@@ -231,17 +245,37 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "web_cam_receiver");
     ros::NodeHandle nh;
 
-    // TODO: make this parameters
+    // get parameters for settings
+    bool all_params_available = true;
+    getParamFatal(frame_id, "frame id");
+    getParamFatal(camera_name, "name of the camera model");
+    getParamFatal(calibration_url, "url to a file with calibration for the given camera (${NAME} expands to camera model name");
+    getParamFatal(image_url, "url from which a single image can be retrieved");
+    getParamFatal(login_info, "login information for the camera in the form user:password");
+    if (! all_params_available)
+    {
+      ROS_FATAL("Unable to start web_cam_receiver because parameters are missing.  "
+                "Make sure that the required paramteres are set in the namespace of this node.");
+      return 1;
+    }
+    float loop_rate_Hz;
+    if (! nh.getParam("loop_rate_Hz", loop_rate_Hz))
+    {
+      loop_rate_Hz = 5;
+      ROS_WARN_STREAM("param loop_rate_Hz not specified; assuming " << loop_rate_Hz);
+    }
+    std::cout << "loop rate: " << loop_rate_Hz << std::endl;
+
     WebCamReceiver receiver(nh,
-                            "/web_cam",
-                            "edimax_ic_3115w",
-                            "package://web_cam_receiver/calib/${NAME}_sxvga.yml",
-                            "http://10.0.126.9/snapshot.jpg",
-                            "admin:1234");
+                            frame_id,
+                            camera_name,
+                            calibration_url,
+                            image_url,
+                            login_info);
 
 
     // main loop
-    ros::Rate loop_rate(5);
+    ros::Rate loop_rate(loop_rate_Hz);
     while (nh.ok())
     {
       // timeout a little bit shorter than loop rate
@@ -254,6 +288,8 @@ int main(int argc, char** argv)
   {
     std::string msg(e.what());
     ROS_FATAL_STREAM("WebCamReceiver failed with " << msg);
+    return 2;
   }
 
+  return 0;
 }
